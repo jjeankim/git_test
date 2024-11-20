bbe352b7d421bde580af4121841b9fa55ce86016
jjeankim <miyukikj35@gmail.com>
2024-11-20 18:36:37 +0900

    Chore: remote local to repo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/app/styles")],
+    additionalData: `
+    @import "_variables_color";
+    @import "_variables";
+    @import "_mixins";`
+  },
 };
 
 export default nextConfig;
